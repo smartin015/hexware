66dbbbe65cace0cff7748066aa0966e84925eff5
Scott Martin <smartin015@gmail.com>
2025-01-01 21:47:21 -0500

    Add ideas

--- hexware.py
+++ hexware.py
@@ -74,22 +74,24 @@ def low_bowl():
     body, _, _, outside_dia = hex_body(h=VH/2, tile_factor=2)
     return body.translate((-outside_dia, 0, 0))
 
-def plate(tile_factor=2):
+def body_mod(h, tile_factor, translate):
     body, _, _, outside_dia = hex_body(VH/3, tile_factor)
-    return body.translate((-outside_dia, 0, 0))
+    return body.translate([outside_dia*dim for dim in translate])
 
 ecup = espresso_cup()
-ecup_saucer = "TODO 2x tile factor with indent for espresso cup"
+ecup_saucer = body_mod(VH/3, 2, (0,0,0)).translate((0,0,-7)).cut(ecup)
 mug = "TODO 2x tile factor, double height"
 cappucino_cup = "TODO 2x tile factor, espresso height"
 cappucino_saucer = "TODO 3x tile factor, holds cappucino cup"
 bowl = "3x tile factor, cappucino height"
 ramen_bowl = "4x tile factor, 2x height"
 serving_tray = "TODO hex grid CNC bamboo platter that holds stuff"
-small_plate = plate(3)
-dinner_plate = plate(5)
-# sauce_dish = ramekin()
-ramekin = "TODO 2x tile factor, 0.5 espresso height"
-
-
-
+placemat = "TODO CNC hex mat with location of various objects"
+small_plate = body_mod(VH/3, 3, (1,1.3,0))
+dinner_plate = body_mod(VH/3, 5, (1,0,0))
+sauce_dish = "TODO ramekin but lower and wider"
+ramekin = body_mod(VH/2, 1, (2,0,0))
+sushi_dipping_tray = "TODO two ramekins fused together"
+base_tile = "TODO solid hexagonal tiles just for messin"
+cupholder = "triplet of 1x tile factor base tiles, combined"
+table = "TODO hexagonal table with cutouts for the placemats, and covers for them that can be magnetically removed"
